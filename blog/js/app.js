f53251798060a311cff2492f6e0f15fb0e728c64
Lex <yobdc.101@gmail.com>
2017-01-25 10:55:16 +0800

    显示列表

--- blog/js/app.js
+++ blog/js/app.js
@@ -1,9 +1,9 @@
 const HomeView = Vue.extend({
 	template: '<div class="home">\
-	<div class="post" v-for="post in posts">\
+	<div class="post row" v-for="post in posts">\
 	<div class="post-meta col-xs-2">{{post.date}}</div>\
 	<div class="post-title col-xs-10">\
-	<a>{{post.title}}</a>\
+	<a v-bind:href="post.url">{{post.title}}</a>\
 	</div>\
 	</div>\
 	</div>',
@@ -23,33 +23,35 @@ const HomeView = Vue.extend({
 					if (postArray && postArray.length == 2) {
 						post.date = postArray[0];
 						post.title = postArray[1];
+						post.url = "/#/post/" + post.date + "/" + post.title;
 					}
 				}
 			}
 		});
 	}
 })
-const Post = {
-	template: '<div>blog text</div>'
-};
+const Post = Vue.extend({
+	template: '',
+	data: function() {
+		return {
+			posts: [{}]
+		}
+	},
+	created: function() {
+		var that = this;
+	}
+});
 
-// 3. 创建 router 实例，然后传 `routes` 配置
-// 你还可以传别的配置参数, 不过先这么简单着吧。
 const router = new VueRouter({
 	routes: [{
 		path: '/',
 		component: HomeView
 	}, {
-		path: '/blog/:filename',
+		path: '/post/:date/:title',
 		component: Post
 	}]
 })
 
-// 4. 创建和挂载根实例。
-// 记得要通过 router 配置参数注入路由，
-// 从而让整个应用都有路由功能
 const app = new Vue({
 	router
-}).$mount('#app')
-
-// 现在，应用已经启动了！
+}).$mount('#app');
