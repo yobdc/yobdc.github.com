97571133a6c56563ec35588359cefd9aff45d301
Lex <yobdc.101@gmail.com>
2017-02-04 12:00:28 +0800

    合并HomeView和TagView

--- blog/js/app.js
+++ blog/js/app.js
@@ -24,79 +24,15 @@ const HomeView = Vue.extend({
 	methods: {
 		init: function() {
 			var that = this;
-			NProgress.start();
-			$.get(blogJsonUrl, function(data) {
-				that.posts = data.posts;
-				for (var i = 0; data.posts && i < data.posts.length; i++) {
-					var post = data.posts[i];
-					if (post && post.file) {
-						var postArray = post.file.split("__");
-						if (postArray && postArray.length == 2) {
-							post.date = postArray[0];
-							post.title = postArray[1];
-							post.url = "/#/post/" + post.date + "/" + post.title;
-							var tags = post.tags;
-							post.tags = [];
-							if (tags) {
-								tags.sort();
-							}
-							for (var j = 0; tags && j < tags.length; j++) {
-								var tag = tags[j];
-								if (tag) {
-									post.tags.push({
-										name: tag,
-										url: '/#/tag/' + tag
-									});
-								}
-							}
-						}
-					}
-				}
-				that.posts.sort(function(a, b) {
-					return a.date < b.date;
-				});
-				NProgress.done();
-			});
-		}
-	},
-	created: function() {
-		this.init();
-	}
-});
-const TagView = {
-	template: '<div class="home">\
-	<div class="post row" v-for="post in posts">\
-	<div class="post-meta">{{post.date}}</div>\
-	<div class="post-title">\
-	<a v-bind:href="post.url">{{post.title}}</a>\
-	</div>\
-	<div class="post-tags hidden-xs">\
-	<a v-bind:href="tag.url" v-for="tag in post.tags">#{{tag.name}}</a>\
-	</div>\
-	</div>\
-	</div>',
-	data: function() {
-		return {
-			posts: []
-		}
-	},
-	watch: {
-		'$route': function(to, from) {
-			this.init();
-		}
-	},
-	methods: {
-		init: function() {
-			var that = this;
 			var params = that.$route.params;
 			var tag = params.tag;
 			NProgress.start();
+			that.posts = [];
 			$.get(blogJsonUrl, function(data) {
-				that.posts = [];
 				for (var i = 0; data.posts && i < data.posts.length; i++) {
 					var post = data.posts[i];
 					var tags = post.tags;
-					if (!tags || tags.indexOf(tag) < 0) {
+					if (tag && tags.indexOf(tag) < 0) {
 						continue;
 					}
 					if (post && post.file) {
@@ -106,6 +42,7 @@ const TagView = {
 							post.title = postArray[1];
 							post.url = "/#/post/" + post.date + "/" + post.title;
 							that.posts.push(post);
+							var tags = post.tags;
 							post.tags = [];
 							if (tags) {
 								tags.sort();
@@ -132,7 +69,7 @@ const TagView = {
 	created: function() {
 		this.init();
 	}
-};
+});
 const PostView = Vue.extend({
 	template: '<div class="post-content">\
 	<div class="post-title">{{post.title}}</div>\
@@ -197,7 +134,7 @@ const router = new VueRouter({
 		component: PostView
 	}, {
 		path: '/tag/:tag',
-		component: TagView
+		component: HomeView
 	}]
 });
 
